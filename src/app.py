9864361eec32f714f77889eadd48fa6fe59ea3c0
Arpita Nayak <arpitanayak050204@gmail.com>
2026-09-01 16:23:12 +0530

    Chatbot ui implementation

--- src/app.py
+++ src/app.py
@@ -4,3 +4,10 @@ st.title("WEB-ASK")
 with st.sidebar:
   st.header("settings")
   website_url = st.text_input("website url")
+
+user_query=st.chat_input("Ask me!!")
+if user_query is not None and user_query !="":
+  with st.chat_message("Human"):
+   st.write(user_query)
+  with st.chat_message("AI"):
+   st.write("Sorry,I don't know")
